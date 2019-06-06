5249f4c2703cbf2fc132fb1b29530224d31cd868
Anna Thomas <annat21@live.co.uk>
2019-06-06 22:24:49 +0100

    Restructured App and imported data

---
+++ NOTES.md
@@ -0,0 +1,20 @@
+### API
+
+Dataset 1 contains:
+  - Categories: `Gender`
+  - Keys: `email`
+
+Dataset 2 contains:
+  - Categories: `Address`
+  - Keys: `UDPRN`, `UDPRN::Forename`, `UDPRN::Forename`
+
+Dataset 3 contains:
+  - Categories: `Custom`, `Address`, `Gender`
+  - Keys: None
+
+
+### App
+
+- Restructured to class syntax
+- Initialise state
+- Import data on lifecycle hook
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
 import './App.css';
 
-import React from 'react';
+import React, { Component } from 'react';
 
+import { Dataset as DatasetDef } from './api/api-definition';
+import getDatasets from './api/api';
 import Dataset from './dataset/Dataset';
 
-function App() {
-  return (
-    <Dataset />
-  );
-}
+interface AppState {
+  data: Array<DatasetDef>
+};
 
-export default App;
+export default class App extends Component<{}, AppState> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    getDatasets.then(data => {
+      this.setState({ data });
+    })
+  }
+
+  render() {
+    const { data } = this.state;
+    console.log(data);
+
+    return (
+      <div>
+        <h1>Datasets Visualisation</h1>
+
+        <Dataset />
+      </div>
+    )
+  }
+}
